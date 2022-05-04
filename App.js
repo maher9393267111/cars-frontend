53cca88f718b410148654e16695ef155148f7534
Your Name <you@example.com>
2022-05-04 16:33:41 +0300

    maher111

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {useState,useRef} from 'react';
 import React from "react";
 import Categories from "./components/categories";
-import Home from "./pages/home";
+//import Home from "./pages/home";
 import PriceRange from './components/priceRange';
 
 function App() {
